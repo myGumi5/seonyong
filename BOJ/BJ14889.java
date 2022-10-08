8e2d9858c223a3459cf5363d688b5748c8913a2a
박선용 <tony3337@naver.com>
2022-10-08 14:51:56 +0900

    2022-10-08

---
+++ BOJ/BJ14889.java
@@ -0,0 +1,90 @@
+package com.ssafy.coding.live20221008;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class BJ14889 {
+	static int N, ans = Integer.MAX_VALUE;
+	static int[][] map;
+	static boolean[] visited;
+	
+	public static void main(String[] args) {
+		FastReader sc = new FastReader();
+		N = sc.nextInt();
+		
+		visited = new boolean[N+1];
+		map = new int[N+1][N+1];
+		for(int i = 1; i <= N; i++) {
+			for(int j = 1; j <= N; j++) {
+				map[i][j] = sc.nextInt();
+			}
+		}
+		
+		dfs(1);
+		System.out.println(ans);
+		
+	}
+	static void dfs(int depth) {
+		if(depth == N + 1) {
+			ArrayList<Integer> link = new ArrayList<>();
+			ArrayList<Integer> start = new ArrayList<>();
+			
+		
+			for(int i = 1; i <= N; i++) {
+				if(visited[i]) link.add(i);
+				else start.add(i);
+			}
+			
+			if(link.size() == N / 2 && start.size() == N/2) {				
+				int linkS = getScore(link);
+				int startS = getScore(start);
+
+				ans = Math.min(ans, Math.abs(linkS - startS));
+			}
+			return;
+		}
+		
+		visited[depth] = true;
+		dfs(depth + 1);
+		visited[depth] = false;
+		dfs(depth+1);
+	}
+
+	private static int getScore(ArrayList<Integer> list) {
+		int sum = 0;
+		for(int i : list) {
+			for(int j : list) {
+				sum += map[i][j];
+			}
+		}
+		
+		return sum;
+	}
+
+	static class FastReader{
+		BufferedReader br = null;
+		StringTokenizer st = null;
+		
+		public FastReader() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+		
+		String next() {
+			while(st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+		
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+		
+	}
+}
