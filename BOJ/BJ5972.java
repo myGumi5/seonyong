0b16cd99471054471479fa4a639856d2cc9e775d
박선용 <tony3337@naver.com>
2022-11-29 21:52:08 +0900

    2022-11-29

---
+++ BOJ/BJ5972.java
@@ -0,0 +1,114 @@
+package com.ssafy.algo.live20221129;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class BJ5972 {
+	static int N, M;
+	static int[] arr;
+	static Map<Integer, ArrayList<Edge>> map = new HashMap<>();
+	
+	public static void main(String[] args) {
+		FastReader sc = new FastReader();
+		
+		N = sc.nextInt();
+		M = sc.nextInt();
+		
+		arr = new int[N+1];
+		Arrays.fill(arr, Integer.MAX_VALUE);
+
+		for(int i = 0; i < M; i++) {
+			int n1 = sc.nextInt();
+			int n2 = sc.nextInt();
+			int c = sc.nextInt();
+			
+			if(map.get(n1) == null) {
+				map.put(n1, new ArrayList<>(Arrays.asList(new Edge(n2, c))));
+			} else {
+				map.get(n1).add(new Edge(n2, c));
+			}
+			if(map.get(n2) == null) {
+				map.put(n2, new ArrayList<>(Arrays.asList(new Edge(n1, c))));
+			} else {
+				map.get(n2).add(new Edge(n1, c));
+			}
+		}
+		
+		bfs();
+		
+		System.out.println(arr[N]);
+		
+	}
+	static void bfs() {
+		PriorityQueue<Edge> pq = new PriorityQueue<>();
+		for(Edge e : map.get(1)) {
+			pq.add(e);
+			arr[e.n] = e.weight;
+		}
+		
+		while(!pq.isEmpty()) {
+			Edge e = pq.poll();
+
+			if(map.get(e.n) == null) continue;
+			for(Edge sub : map.get(e.n)) {
+				int val = arr[e.n] + sub.weight;
+				
+				if(arr[sub.n] > val) {
+					arr[sub.n] = val;
+					pq.add(sub);
+				}
+				
+			}
+		}
+		
+		
+	}
+	
+	static class Edge implements Comparable<Edge>{
+		int n, weight;
+
+		public Edge(int n, int weight) {
+			super();
+			this.n = n;
+			this.weight = weight;
+		}
+
+		@Override
+		public String toString() {
+			return "Edge [n=" + n + ", weight=" + weight + "]";
+		}
+
+		@Override
+		public int compareTo(Edge o) {
+			return this.weight - o.weight;
+		}
+		
+		
+	}
+	
+	static class FastReader{
+		BufferedReader br = null;
+		StringTokenizer st = null;
+		
+		public FastReader() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+		
+		String next() {
+			while(st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+			
+			return st.nextToken();
+		}
+		
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+	}
+}
